e88c3303e5bc6b007ac435cd1603421c06fd27ba
AnandaAp <ananda.apriliansyah@ti.ukdw.ac.id>
2021-09-10 09:33:51 +0700

    penyempurnaan tugas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,23 @@
-var bukuTamu = Buku()
 fun main() {
+    val bukuTamu = Buku()
     while (true){
         println("Selamat Datang")
+        println()
+        println("Menu")
         println("1. Catat Kehadiran")
         println("2. Tampilkan Semua Daftar Tamu")
         println("3. Keluar")
-        print("\nPilihan : ")
+        println()
+        print("Pilihan : ")
         val pilihan = readLine()
         when (pilihan?.toInt()){
             1 -> {
+                println()
                 print("Masukan Nama Anda : ")
                 val nama = readLine().toString()
                 bukuTamu.catatKehadiran(nama)
                 println("Terima Kasih Sudah Berkunjung")
+                println()
             }
             2 -> println(bukuTamu.tampilkan())
             3 -> return
